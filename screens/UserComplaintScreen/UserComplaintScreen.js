b5746a48a272d24072ca5bd76b50a4ac04e5b4e0
ulianacode <uli4n4.4@yandex.ru>
2024-11-24 11:25:56 +0300

    feature(MGDEV-62): template of complaint

---
+++ screens/UserComplaintScreen/UserComplaintScreen.js
@@ -0,0 +1,83 @@
+import {React, useState} from 'react';
+import { View, TouchableOpacity, Text, Image, StyleSheet, StatusBar, TextInput } from 'react-native';
+import styles from './styles';
+import BackButton from '../../components/BackButton/BackButton';
+import { useNavigation } from '@react-navigation/native';
+import { API_URL, tokens } from '../../variables/ip';
+import axios from 'axios';
+
+
+const UserComplaintScreen = () => {
+  const navigation = useNavigation();
+  const [description, setDescription] = useState('');
+  const [lineCount, setLineCount] = useState(1);
+
+const handleBackPress = () => {
+  navigation.navigate('MyProfile');
+};
+const handleComlaint = async () => {
+  
+  try {
+    const reported = 'user6';
+    
+      const response = await axios.post(`http://${API_URL}/v1/complaints`, 
+        { reported, description},
+        {
+          headers: {
+            Authorization: `Bearer ${tokens.accessToken}`,
+          },
+        }
+        );
+
+      console.log(description);
+      if (response.status === 200) {
+         navigation.navigate('MyProfile');
+      } else {
+          Alert.alert('Ошибка');
+      }
+  } catch (error) {
+      if (error.response) {
+          if (error.response.status === 400) {
+              Alert.alert('Ошибка 400');
+          } 
+      }
+      console.error(error);
+  }
+};
+
+
+  return (
+    <View style={styles.container}>
+
+    <BackButton onPress={handleBackPress} />
+
+    <View style={styles.boxContainer}>
+        <View style={styles.fieldContainer}>
+            <View style={styles.labelContainer}>
+              <Text style={[styles.label, styles.interBold]}>Жалоба на</Text>
+              <Text style={[styles.label, styles.interBold]}>пользователя</Text>
+            </View>
+            <View style={styles.inputContainer}>
+            <TextInput
+            style={styles.input}
+            placeholder="Введите текст жалобы "
+            multiline
+            numberOfLines={5}
+            maxLength={360}
+            value={description}
+            onChangeText={setDescription}
+          />
+            </View>
+
+            <TouchableOpacity onPress={handleComlaint} style={styles.bottomBoxContainer}>
+            <Text style={styles.bottomBoxText}>Отправить</Text>
+        </TouchableOpacity>
+
+        </View>
+    </View>
+</View>
+);
+
+};
+
+export default UserComplaintScreen;
